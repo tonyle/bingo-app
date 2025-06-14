4694117e6e3a308877c9dbf6907d04c401638e17
Tony Le <tonyle.microsoft@gmail.com>
2025-06-14 00:04:27 -0500

    removeConsole on prod

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
 
-export default nextConfig
+export default nextConfig;
