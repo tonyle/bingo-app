36dc1a2e7559f182b1a271c5b8d1bb0ee3612621
Tony Le <tonyle.microsoft@gmail.com>
2025-06-15 23:28:13 -0500

    add Suspense

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
